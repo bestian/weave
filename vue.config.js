0cb7b07134d2368372ce14b5a04b6583868a7361
Bestian Tang 小巴 <bestian@gmail.com>
2022-11-22 12:24:44 +0800

    s

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pwa: {
+    workboxOptions: {
+      exclude: [/\.gitkeep/],
+      skipWaiting: true
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/'
+  ? '/weave/'
   : '/',
   transpileDependencies: true
 })
